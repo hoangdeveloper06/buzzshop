89c1899b2a2695531a87728479dcf4d0765701e7
hoangdeveloper06 <56692700+hoangdeveloper06@users.noreply.github.com>
2019-10-18 20:15:50 +0700

    a

---
+++ database/seeds/2019_10_18_115004_create_bill_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_details', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('quantity');
+            $table->double('unit_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill_details');
+    }
+}
